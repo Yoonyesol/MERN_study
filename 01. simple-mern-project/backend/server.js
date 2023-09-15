ad28fdc4bd72ccfa2c10215959e21dcf4e661ead
yesol <maniac970430@naver.com>
2023-09-15 16:28:57 +0900

    fe-be 전체적인 코드 흐름 살펴보기

--- 01. simple-mern-project/backend/server.js
+++ 01. simple-mern-project/backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const uuid = require('uuid/v4');
+const express = require("express");
+const bodyParser = require("body-parser");
+const uuid = require("uuid/v4");
 
 const app = express();
 
@@ -10,42 +10,48 @@ app.use(bodyParser.json());
 
 // CORS Headers => Required for cross-origin/ cross-server communication
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
   );
   next();
 });
 
-app.get('/products', (req, res, next) => {
+//엔드포인트. 특정 경로에 미들웨어 등록
+app.get("/products", (req, res, next) => {
+  //메서드와 경로의 조합에 요청이 도달할 때 실행되는 함수
   res.status(200).json({ products: DUMMY_PRODUCTS });
+  //응답으로 products 반환
 });
 
-app.post('/product', (req, res, next) => {
+//엔드 포인트 --> 백엔드 주요 로직
+app.post("/product", (req, res, next) => {
   const { title, price } = req.body;
 
+  //사용자 입력값 검증
   if (!title || title.trim().length === 0 || !price || price <= 0) {
     return res.status(422).json({
-      message: 'Invalid input, please enter a valid title and price.'
+      message: "Invalid input, please enter a valid title and price.",
     });
   }
 
   const createdProduct = {
     id: uuid(),
     title,
-    price
+    price,
   };
 
-  DUMMY_PRODUCTS.push(createdProduct);
+  DUMMY_PRODUCTS.push(createdProduct); //서버를 끌 때 모든 데이터가 소실된다 -> DB필요!
 
+  //상품이 성공적으로 생성되면 응답을 반환한다.
   res
     .status(201)
-    .json({ message: 'Created new product.', product: createdProduct });
+    .json({ message: "Created new product.", product: createdProduct });
 });
 
 app.listen(5000); // start Node + Express server on port 5000
--- 01. simple-mern-project/frontend/src/App.js
+++ 01. simple-mern-project/frontend/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Header from './components/Header/Header';
-import NewProduct from './components/Products/NewProduct';
-import ProductList from './components/Products/ProductList';
-import './App.css';
+import Header from "./components/Header/Header";
+import NewProduct from "./components/Products/NewProduct";
+import ProductList from "./components/Products/ProductList";
+import "./App.css";
 
 function App() {
   const [loadedProducts, setLoadedProducts] = useState([]);
@@ -12,10 +12,12 @@ function App() {
   useEffect(() => {
     const fetchProducts = async () => {
       setIsLoading(true);
-      const response = await fetch('http://localhost:5000/products');
+      //http 요청(get)
+      const response = await fetch("http://localhost:5000/products");
 
       const responseData = await response.json();
 
+      //백엔드로부터 가져온 데이터를 상태 데이터에 저장
       setLoadedProducts(responseData.products);
       setIsLoading(false);
     };
@@ -23,21 +25,23 @@ function App() {
     fetchProducts();
   }, []);
 
+  //product 객체를 구축하여 백엔드로 전송
   const addProductHandler = async (productName, productPrice) => {
     try {
       const newProduct = {
         title: productName,
-        price: +productPrice // "+" to convert string to number
+        price: +productPrice, // "+" to convert string to number
       };
       let hasError = false;
-      const response = await fetch('http://localhost:5000/product', {
-        method: 'POST',
-        body: JSON.stringify(newProduct),
+      const response = await fetch("http://localhost:5000/product", {
+        method: "POST",
+        body: JSON.stringify(newProduct), //데이터 첨부
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json", //json데이터 전송을 백엔드에게 알려줌
+        },
       });
 
+      //응답이 ok인지 확인
       if (!response.ok) {
         hasError = true;
       }
@@ -48,14 +52,15 @@ function App() {
         throw new Error(responseData.message);
       }
 
-      setLoadedProducts(prevProducts => {
+      //정상적으로 응답을 받았다면 프론트엔드에서 작업한 products를 업데이트해서 UI를 리렌더링하고 새로 추가된 상품을 반영한다.
+      setLoadedProducts((prevProducts) => {
         return prevProducts.concat({
           ...newProduct,
-          id: responseData.product.id
+          id: responseData.product.id,
         });
       });
     } catch (error) {
-      alert(error.message || 'Something went wrong!');
+      alert(error.message || "Something went wrong!");
     }
   };
 
