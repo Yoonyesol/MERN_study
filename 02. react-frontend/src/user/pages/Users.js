d42f0690a8d18d48b3e6bb47cac5d17223067a2f
yesol <maniac970430@naver.com>
2023-09-16 13:38:13 +0900

    앱 스타일링 및 기타 컴포넌트(Card, Avatar)

--- 02. react-frontend/src/user/pages/Users.js
+++ 02. react-frontend/src/user/pages/Users.js
@@ -10,7 +10,7 @@ const Users = () => {
         "https://image.utoimage.com/preview/cp872722/2022/12/202212008462_206.jpg",
       places: 4,
     },
-  ]; //백엔드 생성 전까지 사용할 더미 데이터
+  ];
 
   return <UsersList items={USERS} />;
 };
