3fc1c169349b1c1ed21d4737ad8fdf54162320d7
yesol <maniac970430@naver.com>
2023-09-16 11:43:06 +0900

    UserList, UserItem 페이지/컴포넌트 추가 및 화면에 UserItem 정보 띄우기

--- 02. react-frontend/src/App.js
+++ 02. react-frontend/src/App.js
@@ -8,19 +8,13 @@ function App() {
   return (
     <Router>
       <Switch>
-        {/* 일치하는 경로를 발견하면 그 밑의 코드는 읽지 않는다. */}
         <Route path="/" exact>
-          {/* 
-        exact 없는 경우: 어떤 경로든 /로 시작하기만 하면 Users를 렌더링한다.  
-        exact 없는 경우: /인 경우에만 컴포넌트 렌더링
-        */}
           <Users />
         </Route>
         <Route path="/places/new" exact>
           <NewPlace />
         </Route>
         <Redirect to="/" />
-        {/* 지원하지 않는 페이지로 이동하지 않게 하기 위해 추가 */}
       </Switch>
     </Router>
   );
--- 02. react-frontend/src/user/pages/Users.js
+++ 02. react-frontend/src/user/pages/Users.js
@@ -1,6 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "testUser",
+      image:
+        "https://image.utoimage.com/preview/cp872722/2022/12/202212008462_206.jpg",
+      places: 4,
+    },
+  ]; //백엔드 생성 전까지 사용할 더미 데이터
+
+  return <UsersList items={USERS} />;
 };
 export default Users;
