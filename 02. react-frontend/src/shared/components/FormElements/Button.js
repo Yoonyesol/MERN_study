c49c61df6f437ae28287a322292063da4dfa5a65
yesol <maniac970430@naver.com>
2023-09-16 21:30:35 +0900

    사용자 지정 버튼 추가하기

---
+++ 02. react-frontend/src/shared/components/FormElements/Button.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Button.css";
+
+const Button = (props) => {
+  if (props.href) {
+    //href가 존재한다면 일반 링크 렌더링
+    return (
+      <a
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
+        href={props.href}
+      >
+        {props.children}
+      </a>
+    );
+  }
+  if (props.to) {
+    //react-router-dom에서 나오는 링크를 렌더링
+    return (
+      <Link
+        to={props.to}
+        exact={props.exact}
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
+      >
+        {props.children}
+      </Link>
+    );
+  }
+  return (
+    <button
+      className={`button button--${props.size || "default"} ${
+        props.inverse && "button--inverse"
+      } ${props.danger && "button--danger"}`}
+      type={props.type}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
