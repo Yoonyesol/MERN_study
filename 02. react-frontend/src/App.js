02f19d281fec3855199cec291c5938d4d546c32d
yesol <maniac970430@naver.com>
2023-09-18 01:25:07 +0900

    앱 전반에 걸친 상태 관리를 위한 Auth 컨텍스트 추가, NavLink에서 컨텍스트 상태에 따라 메뉴 표시

--- 02. react-frontend/src/App.js
+++ 02. react-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -12,32 +12,48 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //재생성할 필요 없음, 의존성 빈 배열
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId" exact>
-            <UpdatePlace />
-          </Route>
-          <Route path="/auth" exact>
-            <Auth />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/:userId/places" exact>
+              <UserPlaces />
+            </Route>
+            <Route path="/places/new" exact>
+              <NewPlace />
+            </Route>
+            <Route path="/places/:placeId" exact>
+              <UpdatePlace />
+            </Route>
+            <Route path="/auth" exact>
+              <Auth />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
--- 02. react-frontend/src/shared/components/Navigation/NavLinks.js
+++ 02. react-frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
+import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
+  //컨텍스트 수신하기
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
@@ -11,15 +15,21 @@ const NavLinks = (props) => {
           ALL USESRS
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">MY PLACES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">ADD PLACES </NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACES </NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
