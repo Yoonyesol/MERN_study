cee023d2d3f181b41a774dba7f23e7d725189142
yesol <maniac970430@naver.com>
2023-09-18 03:50:45 +0900

    node.js로 서버 생성하기

--- 03. node-basic/app.js
+++ 03. node-basic/app.js
@@ -1,11 +1,27 @@
-const fs = require('fs');
+const http = require("http");
 
-const userName = 'Max';
+const server = http.createServer((req, res) => {
+  console.log("INCOMING REQUEST");
+  console.log(req.method, req.url);
 
-fs.writeFile('user-data.txt', 'Name: ' + userName, (err) => {
-  if (err) {
-    console.log(err);
-    return;
+  if (req.method === "POST") {
+    //양식이 제출된 경우
+    let body = "";
+    //요청의 모든 본문이 파싱될 때(전체 요청이 도착하면) 실행됨
+    req.on("end", () => {
+      const userName = body.split("=")[1];
+      res.end("<h1>" + userName + "</h1>");
+    });
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+    );
   }
-  console.log('WROTE FILE');
 });
+
+server.listen(5000);
