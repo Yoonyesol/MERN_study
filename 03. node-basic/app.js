b4984d75b5f08569249ac06a8d172f10a336da80
yesol <maniac970430@naver.com>
2023-09-18 10:50:47 +0900

    Express.js의 장점 이해하기(+nodemon)

--- 03. node-basic/app.js
+++ 03. node-basic/app.js
@@ -1,29 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 //응답을 보내는 대신 다른 작업을 하는 미들웨어
-app.use((req, res, next) => {
-  let body = "";
-  //수신 req.body파싱이 완료되면 실행되는 end 리스너
-  req.on("end", () => {
-    const userName = body.split("=")[1];
-    if (userName) {
-      req.body = { name: userName };
-    }
-    next(); //요청을 다음 줄에 있는 미들웨어로 포워딩, 다음 미들웨어 시작하기
-  });
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//POST
+app.post("/user", (req, res, next) => {
+  res.send("<h1> User:" + req.body.username + "</h1>");
 });
 
-app.use((req, res, next) => {
-  if (req.body) {
-    return res.send("<h1>" + req.body.name + "</h1>");
-  }
+//GET
+app.get("/", (req, res, next) => {
   res.send(
-    "<form method='POST'><input type='text' name='username'><button type='submit'>Create User</button></form > "
+    "<form action='/user' method='POST'><input type='text' name='username'><button type='submit'>Create User</button></form > "
   );
 });
 
