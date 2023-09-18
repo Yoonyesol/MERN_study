2895ff9e54c12aa1c99125c95699bac78b1c2d82
yesol <maniac970430@naver.com>
2023-09-18 16:30:07 +0900

    사용자 라우트 설정(가입, 로그인, 사용자 가져오기)

--- 04. node-backend/app.js
+++ 04. node-backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const usersRoute = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoute);
 
 app.use((req, res, next) => {
   const error = new HttpError("라우트를 찾지 못했습니다.", 404);
---
+++ 04. node-backend/controllers/users-controller.js
@@ -0,0 +1,46 @@
+const { v4: uuid } = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  { id: "u1", name: "john", email: "test@google.com", password: "12345" },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (hasUser) {
+    throw new HttpError("이미 등록된 이메일이 존재합니다.", 422);
+  }
+
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError("로그인 정보가 일치하지 않습니다.", 401);
+  }
+  res.json({ message: "로그인 성공!" });
+};
+
+exports.getUsers = getUsers;
+exports.login = login;
+exports.signup = signup;
