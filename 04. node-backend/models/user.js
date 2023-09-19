29bc4c1b685c04f49c1080c522f9d60a3b17c1db
yesol <maniac970430@naver.com>
2023-09-19 16:46:17 +0900

    사용자 모델 생성하기

---
+++ 04. node-backend/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: { type: String, required: true },
+});
+
+//이메일이 기존에 없을 때만 사용자를 생성할 수 있다.
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
